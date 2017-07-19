6aa60b0b70536820dffb878282ad51f1cf7948b9
Andrew Vogel <andrewpvogel@gmail.com>
2017-07-19 15:43:53 -0400

    Add image and date to post-item.

--- app/models/post.js
+++ app/models/post.js
@@ -3,4 +3,10 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   content: DS.attr('string'),
+  posted_date: DS.attr('string'),
+  image: DS.attr('string'),
+
+  hasImage: function() {
+    return !this.get('image');
+  }
 });
--- tests/integration/components/post-item-test.js
+++ tests/integration/components/post-item-test.js
@@ -2,17 +2,19 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import Ember from 'ember';
 
-let post = Ember.Object.create({ title: 'Test Post', content: '<p>This is a test post.</p>' });
+let post = Ember.Object.create({ title: 'Test Post', content: '<p>This is a test post.</p>', image: 'http://via.placeholder.com/350x150', hasImage: true, posted_date: "9/9/1999 06:05 PM" });
 
 moduleForComponent('post-item', 'Integration | Component | post item', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it renders a post', function(assert) {
   this.set('postItem', post);
   this.render(hbs`
     {{post-item post=postItem}}
   `);
 
-  assert.equal(this.$('.panel-heading').text().trim(), 'Test Post');
+  assert.equal(this.$('.post-title').text().trim(), 'Test Post');
+  assert.equal(this.$('.post-image').length, 1);
+  assert.equal(this.$('.post-date').text().trim(), "9/9/1999 06:05 PM");
 });
